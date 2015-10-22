9cb5654f631109fcc9c106f2f3719961bae7f461
Sam Englander <sizeoverload@gmail.com>
2015-10-21 20:43:01 -0400

    first commmit

---
+++ BatchRename.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BatchRename : ScriptableObject {
+	
+	[MenuItem ("Samelot/BatchRename")]
+		static void gorb() { // Renames all selected items in the Hierarchy to the first item selected, with numbers.
+		string iname;
+		string ispacer = "0"; 
+		
+		int icount = 0; 
+		iname = Selection.activeGameObject.name;   // The item in the inspector
+		GameObject[] objs = Selection.gameObjects;
+//		istuff = Selection.gameObjects.length;  // if I wanted this to support renaming of > 99 objects correctly, I'd use this. 
+		foreach (GameObject obj in objs) {
+//			icount ++; 
+//			if (icount > 9) {
+//				ispacer = ""; 
+//			}
+			obj.name = iname + "-" + ispacer + icount; 
+			icount ++;
+		}
+	}	
+}
---
+++ DuplicatePrefab.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+	
+public class DuplicatePrefab : ScriptableObject {
+	[MenuItem("Samelot/DuplicateSelected")]
+	public static void DuplicateSelected ()
+	{
+		GameObject clone;
+		Transform t = Selection.activeTransform;
+		int index = t.GetSiblingIndex ();
+		Object prefabRoot = PrefabUtility.GetPrefabParent (Selection.activeGameObject);
+		
+		if (prefabRoot != null) {
+			PrefabUtility.InstantiatePrefab (prefabRoot);
+		} else {
+			clone = Instantiate (Selection.activeGameObject);	
+			clone.transform.SetSiblingIndex (index);
+			clone.name = clone.name.Replace("(Clone)", "");
+		}
+	}
+}
+
+
+
